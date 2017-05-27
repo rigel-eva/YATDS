7c7f54f6b8f63e5d2f9244b999f5a74afdbddb38
rigel-eva <eva@rigel.moe>
2017-05-27 00:07:43 -0500

    And Hey! It’s working!

--- starfield.js
+++ starfield.js
@@ -23,14 +23,15 @@ class starfield{
     //And putting our image data to the canvas
     canvasContext.putImageData(imageData,0,0)
     //first we need to move our stars down by 1
-    for(var i=this.height-1; i>1; i--){//This increments our height
-      for(var j=this.width-1; j>0; j--){//and this increments our width
-        this.starArray[i*this.width+j]=this.starArray[(i*this.width-1)+j]
+
+    for(var i=this.height-1; i>0; i--){//This increments our height
+      for(var j=this.width-1; j>=0; j--){//and this increments our width
+        this.starArray[i*this.width+j]=this.starArray[(i-1)*this.width+j]
       }
     }
     //and finally, lets generate our next row
     for(var i=0; i<this.width;i++){
-      this.starArray[i]=1==Math.floor(Math.random()*20)//making it so there is a 1 in 20 chance for a star being generated
+      this.starArray[i]=1==Math.floor(Math.random()*100)//making it so there is a 1 in 20 chance for a star being generated
     }
   }
 }
