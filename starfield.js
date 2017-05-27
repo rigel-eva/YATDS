71497492422478976a359bc473ff5737321641ca
rigel-eva <eva@rigel.moe>
2017-05-26 21:47:57 -0500

    Welp … It’s kinda working …
    Still not scrolling down, but Hey, Progress!

--- starfield.js
+++ starfield.js
@@ -1,5 +1,4 @@
 class starfield{
-
   constructor(width, height){
     this.width=width;
     this.height=height;
@@ -9,21 +8,20 @@ class starfield{
     }
   }
   draw(canvasContext){
-    console.log(this.width+" "+this.height)
-    var imageData=canvasContext.createImageData(this.width,this.height)
+    var imageData=canvasContext.getImageData(0,0,width,height)
     //First: let's draw our array
     for(var i=0;i<this.starArray.length;i++){
       if(this.starArray[i]){
         //setting that pixel to white
-        imageData[i*4]=255
-        imageData[i*4+1]=255
-        imageData[i*4+2]=255
-        imageData[i*4+3]=255
+        imageData.data[i*4]=255
+        imageData.data[i*4+1]=255
+        imageData.data[i*4+2]=255
+        imageData.data[i*4+3]=255
       }
 
     }
     //And putting our image data to the canvas
-    canvasContext.getImageData(0,0,width,height)
+    canvasContext.putImageData(imageData,0,0)
     //first we need to move our stars down by 1
     for(var i=this.height-1; i>1; i--){//This increments our height
       for(var j=this.width-1; j>0; j--){//and this increments our width
@@ -33,9 +31,6 @@ class starfield{
     //and finally, lets generate our next row
     for(var i=0; i<this.width;i++){
       this.starArray[i]=1==Math.floor(Math.random()*20)//making it so there is a 1 in 20 chance for a star being generated
-      if(this.starArray[i]){
-        console.log(1)
-      }
     }
   }
 }
