abaaff3348bbd28e91ce2e0c23d8963cb827676f
rigel-eva <eva@rigel.moe>
2017-05-29 18:26:18 -0500

    Made the controls feel just a bit tighter by adding them to a timer!

--- playerShip.js
+++ playerShip.js
@@ -5,28 +5,30 @@ class playerShip{
     this.cHeight=cHeight;
     this.sprite=sprite
     this.x=this.cWidth/2-this.sprite.width/2;
-    this.y=this.cWidth/2//Technicly this works for now ... though I would like to change it ...
+    this.y=this.cHeight/6*5
+    window.setInterval(function(){
+      if(this.stateMachine.up){
+        if(this.y>0){
+            this.y-=1
+        }
+      }else if(this.stateMachine.down){
+        if(this.y+this.sprite.height<this.cHeight){
+            this.y+=1
+        }
+      }
+      if(this.stateMachine.right){
+        if(this.x+this.sprite.width<this.cWidth){
+          this.x+=1
+        }
+      }else if(this.stateMachine.left){
+        if(this.x>0){
+          this.x-=1
+        }
+      }
+    }.bind(this),10)
   }
+
   draw(canvasContext){
-    //First, let's get the state of the state machine, and determine how we should update
-    if(this.stateMachine.up){
-      if(this.y>0){
-          this.y-=1
-      }
-    }else if(this.stateMachine.down){
-      if(this.y+this.sprite.height<this.cHeight){
-          this.y+=1
-      }
-    }
-    if(this.stateMachine.right){
-      if(this.x+this.sprite.width<this.cWidth){
-        this.x+=1
-      }
-    }else if(this.stateMachine.left){
-      if(this.x>0){
-        this.x-=1
-      }
-    }
     //Then, let's actually render our ship
     canvasContext.drawImage(this.sprite,this.x,this.y)
   }
