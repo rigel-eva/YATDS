3b1b1887d4c12e469026c581ebb63bc39bfee548
rigel-eva <eva@rigel.moe>
2017-06-20 23:29:24 -0500

    Seperated out levels Ok, did a bit of work here, I essentially moved all the engine pieces to their own folder (scripts/YATDSEngine) and the level bits to their own folder (scripts/levels) *Fixed starfield so you don't start out in a black void *added function to gameLoop.js that replace .svg files oh, and I should have mentioned this a few commits ago, This is now using JQuery *spookynoises.wav* So, expect more shenanigans with that!

--- assets/scripts/🎚️🕹️.js
+++ assets/scripts/🎚️🕹️.js
@@ -2,6 +2,6 @@
 //First let's go ahead and and remove any extranious paths
 $("svg").children().remove()
 //Then let's go ahead and grab the paths from the file, and just put it in the svg!
-$.get( "./enemyPath.svg",function(data){
+$.get( "/assets/curves/enemyPath.svg",function(data){
   $("svg").append($(data).find("path"))
 })
